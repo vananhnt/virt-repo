31b531281b89e23b20495f4c52b058ca7c8d98c8
vananhnt <vananhnt97@gmail.com>
2022-01-13 07:05:46 +0900

    add java

---
+++ HelloJNI.java
@@ -0,0 +1,24 @@
+
+
+public class HelloJNI {
+	static {
+        System.loadLibrary("hello");    // loads libhello.so
+    }
+
+    private native void sayHello(String name);
+    
+    public void test(int x, int y) {
+		if (x >= 0 && x > y && y == Math.pow(x,2.0)) {
+			sayHello("S0");
+			//System.out.println("S0");
+		}
+		else 
+			//System.out.println("S1");
+			sayHello("S2");
+}
+    public static void main(String[] args) {
+    	(new HelloJNI()).test(2, 4);
+    }
+}
+
+
