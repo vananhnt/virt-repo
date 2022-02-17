5e653d5c4fe3b1fcc7f655a575b44838a692772a
vananhnt <vananhnt97@gmail.com>
2022-02-17 17:36:13 -0500

    one function

--- HelloJNI.java
+++ HelloJNI.java
@@ -12,13 +12,13 @@ public class HelloJNI {
     public void test(int x, int y) {
     	if (x >= 0 && x > y && y == Math.pow(x,2.0)) {
 			x = incr(x);
-    		print("S1");
+    		
 		}
 		else {
 			y = incr(y);
-			print("S2");
+			
 		}
-    	 	print(Integer.toString(y));
+    	 	
 		}
     public static void main(String[] args) {
     	(new HelloJNI()).test(2, 4);
